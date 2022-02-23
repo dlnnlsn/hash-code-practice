d35fa0006ec3ce448453f36242cd7876992b07b1
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-02-23 22:50:38 +0200

    reimplement hill climbing with bitset from standard library because there's a bug somewhere in my implementation

--- 2022-practice-round/hill_climbing.cpp
+++ 2022-practice-round/hill_climbing.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <bitset>
 #include "heuristics.h"
 #include <iostream>
 #include <random>
@@ -11,216 +12,153 @@ using namespace std;
 
 constexpr int pool_size = 1000;
 constexpr double bit_flip_probability = 0.5;
-constexpr double client_satisfaction_probability = 0.33;
+constexpr double client_satisfaction_probability = 0.5;
 
 bool running = true;
 bool evolution_started = false;
 
 void sigint_handler(int sig) {
 	running = false;
-	if (!evolution_started) {
-		exit(0);
-	}
+	if (!evolution_started) exit(0);
 }
 
-typedef struct BitSet {
-	vector<uint64_t> bits;
-	BitSet(const vector<uint64_t>& bits) : bits(bits) {}
-	BitSet(const size_t size) {
-		const size_t blocks = (size >> 6) + (((size & 63) > 0) ? 1 : 0);
-		bits = vector<uint64_t>(blocks, 0);
-	}
-
-	friend BitSet operator&(const BitSet& l, const BitSet& r) {
-		vector<uint64_t> result;
-		result.reserve(l.bits.size());
-		for (size_t i = 0; i < l.bits.size(); ++i) result.push_back(l.bits[i] & r.bits[i]);
-		return BitSet(result);
-	}
-
-	friend BitSet operator|(const BitSet& l, const BitSet& r) {
-		vector<uint64_t> result;
-		result.reserve(l.bits.size());
-		for (size_t i = 0; i < l.bits.size(); ++i) result.push_back(l.bits[i] | r.bits[i]);
-		return BitSet(result);
-	}
-
-	friend bool operator==(const BitSet& l, const BitSet& r) {
-		for (int i = 0; i < l.bits.size(); ++i) {
-			if (l.bits[i] != r.bits[i]) return false;
-		}
-		return true;
-	}
-
-	friend BitSet operator~(const BitSet& bits) {
-		vector<uint64_t> result;
-		result.reserve(bits.bits.size());
-		for (size_t i = 0; i < bits.bits.size(); ++i) result.push_back(~bits.bits[i]);
-		return BitSet(result);
-	}
-
-	bool get(const size_t& index) {
-		const uint64_t block = bits[index >> 6];
-		return ((block >> (index & 63)) & 1) == 1;
-	}
-
-	void set(const size_t index, const bool value) {
-		if (value) {
-			bits[index >> 6] |= (1 << (index & 63));
-		}
-		else {
-			bits[index >> 6] &= ~(1 << (index & 63));
+struct seed {
+	typedef unsigned int result_type;
+	random_device dev;
+	template <class RandomAccessIterator>
+	void generate(RandomAccessIterator begin, RandomAccessIterator end) {
+		for (RandomAccessIterator item = begin; item != end; ++item) {
+			*item = dev();
 		}
 	}
+	seed() : dev{} {}
+};
 
-	void flip(const size_t index) {
-		bits[index >> 6] ^= (1 << (index & 63));
-	}
-
-	bool empty() {
-		for (uint64_t block : bits) {
-			if (block != 0) return false;
-		}
-		return true;
-	}
-} BitSet;
+typedef bitset<10000> bits;
 
 typedef struct Gene {
-	BitSet ingredients;
-	int fitness;
-	Gene(BitSet ingredients, int fitness) : ingredients(ingredients), fitness(fitness) {}
-	friend constexpr bool operator<(const Gene& a, const Gene& b) { 
-		if (a.fitness != b.fitness) return a.fitness < b.fitness; 
-		for (size_t i = 0; i < a.ingredients.bits.size(); ++i) {
-			if (a.ingredients.bits[i] != b.ingredients.bits[i]) return a.ingredients.bits[i] < b.ingredients.bits[i];
+	bits ingredients;
+	size_t fitness;
+	Gene(bits ingredients, size_t fitness) : ingredients(ingredients), fitness(fitness) {}
+	friend constexpr bool operator<(const Gene& a, const Gene& b) {
+		if (a.fitness != b.fitness) return a.fitness < b.fitness;
+		for (size_t i = 0; i < 10000; ++i) {
+			if (a.ingredients[i] != b.ingredients[i]) return a.ingredients[i] < b.ingredients[i];
 		}
 		return false;
 	}
 } Gene;
 
-const int evaluate_fitness(const BitSet& ingredients, const vector<BitSet>& clientLikes, const vector<BitSet>& clientDislikes) {
-	int satisfied = 0;
-	for (int client = 0; client < clientLikes.size(); ++client) {
-		if ((ingredients & clientLikes[client]) == clientLikes[client]) {
-			if ((ingredients & clientDislikes[client]).empty()) satisfied++;
+const size_t evaluate_fitness(const bits& ingredients, const vector<bits>& client_likes, const vector<bits>& client_dislikes) {
+	size_t satisfied = 0;
+	for (size_t client = 0; client < client_likes.size(); ++client) {
+		if ((ingredients & client_likes[client]) == client_likes[client]) {
+			if ((ingredients & client_dislikes[client]).none()) {
+				++satisfied;
+			}
 		}
 	}
 	return satisfied;
 }
 
 template <class Generator>
-const BitSet random_bitset(Generator& gen, size_t size) {
-	uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
-	const size_t blocks = (size >> 6) + (((size & 63) > 0) ? 1 : 0);
-	vector<uint64_t> bits; bits.reserve(blocks);
-	for (size_t block = 0; block < blocks; ++block) {
-		bits.push_back(dist(gen));
+const bits random_bitset(Generator& generator, size_t num_ingredients) {
+	bits result;
+	bernoulli_distribution dist(0.5);
+
+	for (size_t i = 0; i < num_ingredients; ++i) {
+		result[i] = dist(generator);
 	}
-	return BitSet(bits);
-}
 
-template <class Generator>
-const BitSet flip_random_bit(Generator& gen, const BitSet& bits, const size_t num_bits) {
-	BitSet new_bits(bits.bits);
-	uniform_int_distribution<size_t> dist(0, num_bits - 1);
-	uniform_real_distribution<double> real_dist(0, 1);
-	do {
-		new_bits.flip(dist(gen));
-	} while (real_dist(gen) < bit_flip_probability);
-	return new_bits;
-}
-
-template <class Generator>
-const BitSet satisfy_random_client(Generator& gen, const BitSet& bits, const vector<BitSet>& client_likes, const vector<BitSet>& client_dislikes) {
-	uniform_int_distribution<size_t> dist(0, client_likes.size() - 1);
-	uniform_real_distribution<double> real_dist(0, 1);
-	BitSet result = BitSet(bits.bits);
-	do {
-		const size_t client_index = dist(gen);
-		result = result | client_likes[client_index];
-		result = result & (~client_dislikes[client_index]);
-	} while (real_dist(gen) < client_satisfaction_probability);
 	return result;
 }
 
-BitSet ingredients_from_client_set(const unordered_set<int>& clients, const vector<BitSet>& client_likes, const int num_ingredients) {
-	BitSet ingredients = BitSet(num_ingredients);
-	for (int client : clients) {
-		ingredients = ingredients | client_likes[client];
+const bits ingredients_from_client_set(const unordered_set<size_t>& clients, const vector<bits>& client_likes) {
+	bits ingredients;
+	for (size_t client: clients) {
+		ingredients |= client_likes[client];
 	}
 	return ingredients;
 }
 
+template <class Generator>
+const bits flip_random_bits(Generator& generator, const bits current_bits, const size_t num_ingredients) {
+	bits new_bits(current_bits);
+	uniform_int_distribution<size_t> dist(0, num_ingredients - 1);
+	uniform_real_distribution<double> real_dist(0, 1);
+	do {
+		new_bits.flip(dist(generator));
+	} while (real_dist(generator) < bit_flip_probability);
+	return new_bits;
+}
+
+template <class Generator>
+const bits satisfy_random_clients(Generator& generator, const bits current_bits, const vector<bits>& client_likes, const vector<bits>& client_dislikes) {
+	uniform_int_distribution<size_t> dist(0, client_likes.size() - 1);
+	uniform_real_distribution<double> real_dist(0, 1);
+	bits result = bits(current_bits);
+	do {
+		const size_t client_index = dist(generator);
+		result |= client_likes[client_index];
+		result &= (~client_dislikes[client_index]);
+	} while (real_dist(generator) < client_satisfaction_probability);
+	return result;
+}
+
 int main() {
+
+	struct seed seeder;
+	mt19937_64 generator(seeder);
 
 	signal(SIGINT, sigint_handler);
 
-	random_device dev;
-	mt19937_64 gen(dev());
+	size_t num_clients; cin >> num_clients;
 
-	int C; cin >> C;
-
-	vector<vector<string>> clientLikeNames; clientLikeNames.reserve(C);
-	vector<vector<string>> clientDislikeNames; clientDislikeNames.reserve(C);
-
-	unordered_map<string, int> ingredient_ids;
+	unordered_map<string, size_t> ingredient_ids;
 	vector<string> ingredient_names;
+	size_t num_ingredients = 0;
 
-	for (int client = 0; client < C; ++client) {
-		int L; cin >> L;
-		vector<string> likes; likes.reserve(L);
-		for (int i = 0; i < L; ++i) {
+	vector<bits> client_likes;
+	client_likes.reserve(num_clients);
+	vector<bits> client_dislikes;
+	client_dislikes.reserve(num_clients);
+
+	for (int client = 0; client < num_clients; ++client) {
+		int num_likes; cin >> num_likes;
+		bits current_likes;
+		for (int i = 0; i < num_likes; ++i) {
 			string name; cin >> name;
 			if (ingredient_ids.count(name) == 0) {
-				ingredient_ids[name] = ingredient_names.size();
+				ingredient_ids[name] = num_ingredients++;
 				ingredient_names.push_back(name);
 			}
-			likes.push_back(name);
-		}	
-		clientLikeNames.push_back(likes);
+			current_likes[ingredient_ids[name]] = true;
+		}
+		client_likes.push_back(current_likes);
 
-		int D; cin >> D;
-		vector<string> dislikes; dislikes.reserve(D);
-		for (int i = 0; i < D; ++i) {
+		int num_dislikes; cin >> num_dislikes;
+		bits current_dislikes;
+		for (int i = 0; i < num_dislikes; ++i) {
 			string name; cin >> name;
 			if (ingredient_ids.count(name) == 0) {
-				ingredient_ids[name] = ingredient_names.size();
+				ingredient_ids[name] = num_ingredients++;
 				ingredient_names.push_back(name);
 			}
-			dislikes.push_back(name);
+			current_dislikes[ingredient_ids[name]] = true;
 		}
-		clientDislikeNames.push_back(dislikes);
+		client_dislikes.push_back(current_dislikes);
 	}
 
-	vector<BitSet> clientLikes; clientLikes.reserve(C);
-	vector<BitSet> clientDislikes; clientDislikes.reserve(C);
+	vector<unordered_set<size_t>> conflict_graph;
+	conflict_graph.reserve(num_clients);
+	for (size_t i = 0; i < num_clients; ++i) conflict_graph.push_back(unordered_set<size_t>());
 
-	for (vector<string> like_names : clientLikeNames) {
-		BitSet likes(ingredient_names.size());
-		for (string name : like_names) {
-			likes.set(ingredient_ids[name], true);
-		}
-		clientLikes.push_back(likes);
-	}
-
-	for (vector<string> dislike_names : clientDislikeNames) {
-		BitSet dislikes(ingredient_names.size());
-		for (string name : dislike_names) {
-			dislikes.set(ingredient_ids[name], true);
-		}
-		clientDislikes.push_back(dislikes);
-	}
-
-	vector<unordered_set<int>> conflictGraph;
-	conflictGraph.reserve(C);
-	for (int i = 0; i < C; ++i) conflictGraph.push_back(unordered_set<int>());
-
-	for (int i = 0; i < C; ++i) {
-		for (int j = 0; j < C; ++j) {
+	for (size_t i = 0; i < num_clients; ++i) {
+		for (size_t j = 0; j < num_clients; ++j) {
 			if (i == j) continue;
-			if (!(clientLikes[i] & clientDislikes[j]).empty()) {
-				conflictGraph[i].insert(j);
-				conflictGraph[j].insert(i);
+			if (!(client_likes[i] & client_dislikes[j]).none()) {
+				conflict_graph[i].insert(j);
+				conflict_graph[j].insert(i);
 			}
 		}
 	}
@@ -229,62 +167,59 @@ int main() {
 
 	vector<Gene> pool; pool.reserve(pool_size);
 
-	BitSet most_conflicting = ingredients_from_client_set(removeMostConflicting(conflictGraph), clientLikes, ingredient_names.size());
-	pool.push_back(Gene(most_conflicting, evaluate_fitness(most_conflicting, clientLikes, clientDislikes)));
+	unordered_set<size_t> most_conflicting = removeMostConflicting(conflict_graph);
+	bits most_conflicting_ingredients = ingredients_from_client_set(most_conflicting, client_likes);
+	cerr << "Most conflicting heuristic: ";
+	cerr << evaluate_fitness(most_conflicting_ingredients, client_likes, client_dislikes) << endl;
 
-	BitSet least_conflicting = ingredients_from_client_set(addLeastConflicting(conflictGraph), clientLikes, ingredient_names.size());
-	pool.push_back(Gene(least_conflicting, evaluate_fitness(least_conflicting, clientLikes, clientDislikes)));
+	unordered_set<size_t> least_conflicting = addLeastConflicting(conflict_graph);
+	bits least_conflicting_ingredients = ingredients_from_client_set(least_conflicting, client_likes);
+	cerr << "Least conflicting heuristic: ";
+	cerr << evaluate_fitness(least_conflicting_ingredients, client_likes, client_dislikes) << endl;
 
-	BitSet least_dislikes = ingredients_from_client_set(leastDislikes(conflictGraph, clientDislikeNames), clientLikes, ingredient_names.size());
-	pool.push_back(Gene(least_dislikes, evaluate_fitness(least_dislikes, clientLikes, clientDislikes)));
-
-	BitSet fewest_preferred = ingredients_from_client_set(fewestPreferences(conflictGraph, clientLikeNames, clientDislikeNames), clientLikes, ingredient_names.size());
-	pool.push_back(Gene(fewest_preferred, evaluate_fitness(fewest_preferred, clientLikes, clientDislikes)));
-
-	for (int i = 0; i < pool_size - 4; ++i) {
-		const BitSet ingredients = random_bitset(gen, ingredient_names.size());
-		const int fitness = evaluate_fitness(ingredients, clientLikes, clientDislikes);
+	for (size_t i = 2; i < pool_size; ++i) {
+		const bits ingredients = random_bitset(generator, num_ingredients);
+		const size_t fitness = evaluate_fitness(ingredients, client_likes, client_dislikes);
 		pool.push_back(Gene(ingredients, fitness));
 	}
+
 	sort(pool.begin(), pool.end());
 
 	evolution_started = true;
-	int generation = 0;
+	size_t generation = 0;
 
 	while (running) {
-		generation++;
-		cerr << "Generation " << generation << ". Best fitness: " << pool.back().fitness << endl;
+		++generation;
+		cerr << "Generation: " << generation << ". Best fitness: " << pool.back().fitness << endl;
 		vector<Gene> new_pool; new_pool.reserve(3 * pool_size);
 		for (Gene gene : pool) {
 			new_pool.push_back(gene);
-			const BitSet random_ingredient_flipped = flip_random_bit(gen, gene.ingredients, ingredient_names.size());
-			new_pool.push_back(Gene(random_ingredient_flipped, evaluate_fitness(random_ingredient_flipped, clientLikes, clientDislikes)));
-			const BitSet random_client_satisfied = satisfy_random_client(gen, gene.ingredients, clientLikes, clientDislikes);
-			new_pool.push_back(Gene(random_client_satisfied, evaluate_fitness(random_client_satisfied, clientLikes, clientDislikes)));
+			const bits random_ingredients_flipped = flip_random_bits(generator, gene.ingredients, num_ingredients);
+			new_pool.push_back(Gene(random_ingredients_flipped, evaluate_fitness(random_ingredients_flipped, client_likes, client_dislikes)));
+			const bits random_clients_satisfied = satisfy_random_clients(generator, gene.ingredients, client_likes, client_dislikes);
+			new_pool.push_back(Gene(random_clients_satisfied, evaluate_fitness(random_clients_satisfied, client_likes, client_dislikes)));
 		}
 		sort(new_pool.begin(), new_pool.end());
 		vector<Gene> filtered_pool; filtered_pool.reserve(3 * pool_size);
 		filtered_pool.push_back(new_pool.front());
 		for (Gene gene : new_pool) {
-			if (!(gene.ingredients == filtered_pool.back().ingredients)) {
+			if (gene.ingredients != filtered_pool.back().ingredients) {
 				filtered_pool.push_back(gene);
 			}
 		}
 		while (filtered_pool.size() < pool_size) {
-			const BitSet ingredients = random_bitset(gen, ingredient_names.size());
-			filtered_pool.push_back(Gene(ingredients, evaluate_fitness(ingredients, clientLikes, clientDislikes)));
+			const bits ingredients = random_bitset(generator, num_ingredients);
+			filtered_pool.push_back(Gene(ingredients, evaluate_fitness(ingredients, client_likes, client_dislikes)));
 		}
+		sort(filtered_pool.begin(), filtered_pool.end());
 		pool = vector<Gene>(filtered_pool.end() - pool_size, filtered_pool.end());
 	}
 
-	BitSet ingredients = pool.back().ingredients;
-	size_t ingredient_count = 0;
-	for (size_t i = 0; i < ingredient_names.size(); ++i) {
-		if (ingredients.get(i)) ingredient_count++;
-	}
-	cout << ingredient_count;
-	for (size_t i = 0; i < ingredient_names.size(); ++i) {
-		if (ingredients.get(i)) cout << " " << ingredient_names[i];
+	cerr << "Writing best solution found..." << endl;
+	bits ingredients = pool.back().ingredients;
+	cout << ingredients.count();
+	for (size_t i = 0; i < num_ingredients; ++i) {
+		if (ingredients[i]) cout << " " << ingredient_names[i];
 	}
 	cout << endl;
 
